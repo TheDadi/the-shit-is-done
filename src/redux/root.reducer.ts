e3a49d6c7e2778d9b72ee3e3df3bc23aecce0c69
dariodifazio <d>
2018-04-09 10:19:00 +0200

    rework typings

--- src/redux/root.reducer.ts
+++ src/redux/root.reducer.ts
@@ -1,11 +1,11 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer, FormState } from 'redux-form';
+import { combineReducers } from 'redux'
+import { reducer as formReducer, FormState } from 'redux-form'
 
 export const reducer = combineReducers<RootState>({
-    form: formReducer
-});
+  form: formReducer,
+})
 
 export interface RootState {
-    form: FormState,
-    test: any,
+  form: FormState,
+  test: any,
 }
